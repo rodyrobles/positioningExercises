e412858f6a95b0a75f9b95cf260f38399a1e9db1
rodyrobles <rodyrob@gmail.com>
2017-04-27 12:14:04 -0500

    Fifth challenge

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var container = document.getElementsByClassName("container")[0];
+var pink = document.getElementById("pink");
+var blue = document.getElementById("blue");
+var green = document.getElementById("green");
+var yellow = document.getElementById("yellow");
+
+container.style.position = "relative";
+container.style.background = "DarkGray";
+container.style.width = "600px";
+container.style.padding = "50px"; 
+
+blue.style.position = "absolute";
+blue.style.top = "250px";
+blue.style.right = "0";
+
